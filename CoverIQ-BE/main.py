f8467de3f4cb947e1fce8524325e15e209547ce2
James Tu <jamestu6301@gmail.com>
2025-06-25 20:56:03 +0800

    update: origins for backend

--- CoverIQ-BE/main.py
+++ CoverIQ-BE/main.py
@@ -7,7 +7,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],  # Vite dev server
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
